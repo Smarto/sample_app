4684bf4ead614906610d3fe4c5ab8b7ab55ce766
Christian Moeller <ceniels@gmail.com>
2011-09-05 03:22:01 +0900

    Updated tests to remove some repetition

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+	@basicTitle = "Ruby on Rails Tutorial Sample App | "
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                        :content => @basicTitle + "Home")
     end
   end
 
@@ -26,7 +30,7 @@ describe PagesController do
       get 'contact'
       response.should have_selector("title",
                         :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                          @basicTitle + "Contact")
     end
   end
 
@@ -40,7 +44,7 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                         :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                          @basicTitle + "About")
     end
   end
 
@@ -53,7 +57,7 @@ describe PagesController do
 	it "should have the right title" do
 		get 'help'
 		response.should have_selector("title",
-					:content => "Ruby on Rails Tutorial Sample App | Help")
+					:content => @basicTitle + "Help")
 	end
   end
 end
